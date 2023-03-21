8b535e896fd2f72fd9632a477f5f52f59adad33c
Jose David Gale <josedavidgale@gmail.com>
2023-03-21 23:32:04 +0000

    Remove hello World

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 Rovers.RoversRobot newRovers = new Rovers.RoversRobot(5, 5);
 Console.WriteLine(newRovers.Instruction("FFRFLFLF"));
